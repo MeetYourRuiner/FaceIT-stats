538fc3fb2ae019760488a3e79ed09706bfe2d479
Stotch <mercer108@gmail.com>
2021-03-16 16:11:08 +0300

    Deleted api constants class -_-

--- tests/IntegrationTests/APIKeys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace IntegrationTests
-{
-    static class APIKeys
-    {
-        public const string FaceitAPIKey = "a847d087-70da-4dd0-992d-cc000f257839";
-        public const string UserAPIKey = "bb80bcc7-6363-4c71-bfcc-36560948710a";
-    }
-}
